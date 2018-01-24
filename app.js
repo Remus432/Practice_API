e95c12390711dce450a2017d1abdd791036b0291
Remus432 <buhaianu_remus@yahoo.com>
2018-01-24 11:11:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const jsonData = require("./public/db.json");
+const cors = require("cors");
 
 // Set port for heroku
 const port = process.env.PORT || 8000;
+
+// Use CORS
+app.use(cors());
 
 // Init App
 const app = express();
